469b5e63aa39f3ceddcc614683303a5d4baabd51
truong12003 <bigg1122003@gmail.com>
2023-10-31 13:35:55 +0700

    init project

---
+++ app/src/main/java/com/example/baitapscreen2/MainActivity.java
@@ -0,0 +1,29 @@
+
+
+package com.example.baitapscreen2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
+	
+	
